4874a8bd08cb5d2938b118dde6abc5032da9f6d3
Marlon Ferreira <94620467+marlonfeerreira@users.noreply.github.com>
2023-02-09 09:04:05 -0300

    Add files via upload

--- Questão10.js
+++ Questão10.js
@@ -1,27 +1,24 @@
 function bhaskara(a, b, c){
-    const delta = b * b - 4 * a * c;
+  var delta = (b**2)- 4*a*c;
+  var raiz = Math.sqrt(delta);
 
-    if(!a || !b || !c){
-        return "Insira os valores de a, b, c";
-    }
-    else if(a == 0){
-        return "O valor de a deve ser diferente de 0";
-    }
-    else if(delta < 0){
-        return array = [];
-    }
-    else if (delta == 0){
-        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        const x2 = (-b + Math.sqrt(delta)) / (2 * a);
-        return array = [x1];
-    }
-    else{
-        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        const x2 = (-b + Math.sqrt(delta)) / (2 * a);
-        return array = [x1, x2];
-    }
+  var x1 = ((-1*b) + raiz)/(2*a);
+  var x2 = ((-1*b) - raiz)/(2*a);
+  
+  if(delta < 0){
+    console.log("[]")
+    var a = []
+    return (a)
+  } else if(delta == 0){
+    x1 = x2;
+    var b = [x1];
+    console.log(b)
+    return(b)
+  } else {
+    var c = [x1, x2];
+    console.log(c)
+    return (c)
+  }
 }
 
-console.log(bhaskara(-3, 18, -15));
-console.log(bhaskara(3, 4, 2));
-console.log(bhaskara(4, -12, 9));
+bhaskara(-3, 18, -15)
